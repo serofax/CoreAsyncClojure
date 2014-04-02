cf7ecde7f94c483197dd95b4147dd237416744b4
Vincent Elliott Wagner <Vincent.Elliott.Wagner@mni.thm.de>
2014-04-02 11:53:35 +0200

    renamed 2 and delete unused folders

--- src/net-channel/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to net-channel
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- src/net-channel/test/net_channel/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns net-channel.core-test
-  (:require [clojure.test :refer :all]
-            [net-channel.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
